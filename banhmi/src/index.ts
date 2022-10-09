3a336fb53539d9fe08502dbc06b7f2f934779580
minhnguyenphamquoc <Minh_Nguyen@mckinsey.com>
2022-10-09 15:17:05 +0700

    feat: add logger

--- banhmi/src/index.ts
+++ banhmi/src/index.ts
@@ -1,18 +1,23 @@
 import "dotenv/config";
 import express from "express";
 import http from "http";
+import pino from "pino-http";
 
 import { sayHello } from "@/utils/greeting";
 
-(async () => {
+const pinoHttp = pino();
+
+const main = async () => {
   sayHello("Minh");
 
   /* Server Setup */
   const app = express();
   const server = http.createServer(app);
 
+  app.use(pinoHttp);
+
   app.get("/", (_req, res) => {
-    res.send("<h1>Welcome to the WebRTC Demo App.");
+    res.send("<h1>Welcome to the WebRTC Demo App.</h1>");
   });
 
   server.listen(process.env.APP_PORT, () => {
@@ -20,4 +25,6 @@ import { sayHello } from "@/utils/greeting";
       `Server listening on ${process.env.APP_HOST}:${process.env.APP_PORT}`
     );
   });
-})().catch((err) => console.error(err));
+};
+
+main().catch((err) => console.error(err));
