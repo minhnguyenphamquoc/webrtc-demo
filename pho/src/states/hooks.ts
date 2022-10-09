cba8278b595c971f44ca7e95da4b955cbda88687
minhnguyenphamquoc <Minh_Nguyen@mckinsey.com>
2022-10-09 15:18:07 +0700

    chore: update package

--- pho/src/states/hooks.ts
+++ pho/src/states/hooks.ts
@@ -4,4 +4,5 @@ import { AppDispatch, RootState } from '@/states/store';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
